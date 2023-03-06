f8f21fca9725218649bd34d4b63e4c33e783d5a2
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2023-03-06 07:04:31 -0300

    Inital Flask and DB configurations

---
+++ coordinates/__init__.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # Create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'address.mongo')
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import address
+    app.register_blueprint(address.bp)
+
+    return app
---
+++ coordinates/db.py
@@ -0,0 +1,20 @@
+import pymongo
+from flask import current_app, g
+
+
+def get_db():
+    if 'db' not in g:
+        client = pymongo.MongoClient(
+            current_app.config['DATABASE']
+        )
+
+        g.db = client['coordinates']
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
