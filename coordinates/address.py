6590d7abb2211963d61ef67c5c48e3246dc3f010
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2023-03-10 20:59:55 -0300

    Including update view and get address function

--- coordinates/address.py
+++ coordinates/address.py
@@ -1,8 +1,8 @@
-import functools
 from flask import (
-    Blueprint, flash, render_template, request
+    Blueprint, flash, render_template, request, abort
 )
 from coordinates.db import get_address_collection
+from bson.objectid import ObjectId
 
 bp = Blueprint('address', __name__, url_prefix='/address')
 
@@ -21,11 +21,39 @@ def get_fields(fields, required_fields):
     return address, error
 
 
+def get_address(address_id):
+    address_collection = get_address_collection()
+    address = address_collection.find_one({'_id': ObjectId(address_id)})
+
+    if address is None:
+        abort(404, 'Address not found')
+
+    return address
+
+
 @bp.route('/')
 def index():
     address_collection = get_address_collection()
     addresses = address_collection.find()
     return render_template('address/index.html', addresses=addresses)
+
+
+@bp.route('/<address_id>/update', methods=('GET', 'POST'))
+def update(address_id):
+    if request.method == 'POST':
+        fields = ['number', 'street', 'district', 'city', 'state', 'country', ]
+        required_fields = fields
+
+        address, error = get_fields(fields, required_fields)
+
+        if error is not None:
+            flash(error)
+        else:
+            address_collection = get_address_collection()
+            address_collection.update_one({'_id': ObjectId(address_id)}, {'$set': address})
+
+    address = get_address(address_id)
+    return render_template('address/update.html', address=address)
 
 
 @bp.route('/register', methods=('GET', 'POST'))
@@ -36,14 +64,10 @@ def register():
 
         address, error = get_fields(fields, required_fields)
 
-        if error is None:
-            address_collection = get_address_collection()
-            try:
-                address_collection.insert_one(address)
-            except Exception as e:
-                error = f'There was an error entering the requested address: {e}'
-
         if error is not None:
             flash(error)
+        else:
+            address_collection = get_address_collection()
+            address_collection.insert_one(address)
 
     return render_template('address/register.html')
