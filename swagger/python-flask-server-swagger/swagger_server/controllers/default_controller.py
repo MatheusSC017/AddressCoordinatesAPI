9a2aa24916744c7dbd6cf8a8d36a538dd3d6d89a
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2023-12-01 06:55:45 -0300

    Copy of Swagger documentation

---
+++ swagger/python-flask-server-swagger/swagger_server/controllers/default_controller.py
@@ -0,0 +1,26 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def api_addresses_get():  # noqa: E501
+    """Returns a list of addresses
+
+     # noqa: E501
+
+
+    :rtype: List[object]
+    """
+    return 'do some magic!'
+
+
+def api_addresses_post():  # noqa: E501
+    """Register an address
+
+     # noqa: E501
+
+
+    :rtype: str
+    """
+    return 'do some magic!'
